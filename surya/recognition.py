356fa0a6bed6151cec606323241bb8cd8a7a0efe
Caleb Moses <calebjdmoses@gmail.com>
2024-06-16 12:43:23 -0400

    Drop lang if image is None

--- surya/recognition.py
+++ surya/recognition.py
@@ -29,7 +29,8 @@ def batch_recognition(images: List, languages: List[List[str]], model, processor
     for l in languages:
         assert len(l) <= settings.RECOGNITION_MAX_LANGS, f"OCR only supports up to {settings.RECOGNITION_MAX_LANGS} languages per image, you passed {l}."
 
-    images = [image.convert("RGB") for image in images] # also copies the images
+    languages = [lang for lang, image in zip(languages, images) if not image is None]
+    images = [image.convert("RGB") for image in images if not image is None] # also copies the images
     if batch_size is None:
         batch_size = get_batch_size()
 
